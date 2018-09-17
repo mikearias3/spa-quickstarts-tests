19746d59804d436a986c9b630f1dda8c2c79a0cc
Michael Arias <michael.arias.93@gmail.com>
2018-09-17 11:36:02 -0400

    .

--- lock_login_test.js
+++ lock_login_test.js
@@ -4,7 +4,7 @@ Scenario('Log in using Lock', (I) => {
     I.amOnPage('/');
     I.waitForVisible('#qsLoginBtn', 3);
     I.click('#qsLoginBtn');
-    I.waitForVisible('input[name="password"]', 5);
+    I.waitForVisible('input[name="password"]', 10);
     I.wait(1);
     I.fillField('input[name="username"]', 'asdasd');
     I.fillField('input[name="password"]', 'asdasd');
